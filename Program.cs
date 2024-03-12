62d6f2566a9f61ff3279a571efb28595708c64a8
Paulo Henrique <paulohenriqueferreirafranca2@gmail.com>
2024-03-12 16:14:57 -0300

    Iniciando projeto Média de Aluno

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MediatrêsNotas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
